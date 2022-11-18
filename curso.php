ed177ae3c06ff2c2a47200e6d4260827d07825ad
DougPrimo2k22 <113148844+DougPrimo2k22@users.noreply.github.com>
2022-11-18 20:13:31 -0300

    Atividades

---
+++ curso.php
@@ -0,0 +1,10 @@
+<p> 8092305 - Douglas Ferreira Primo </p>
+<html>
+<body>
+    <h3>Resultado</h3>
+    <?php
+    echo "<p>Curso selecionado: " . $_POST["curso"] . "</p>\n";
+    ?>
+</body>
+</html>
+
---
+++ recebe_dados.php
@@ -0,0 +1,7 @@
+<p> 8092305 - Douglas Ferreira Primo </p>
+<html>
+<body>
+	Olá <?php echo $_GET["nome"]; ?><br>
+    você tem <?php echo $_GET["idade"]; ?> anos.
+</body>
+</html>
